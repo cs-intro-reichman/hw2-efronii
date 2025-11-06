666bc1a56798cd5785e0e2d6082d806e7e4957b1
rom efroni <efronii14@gmail.com>
2025-11-06 17:41:53 +0200

    plzzzahahaha

--- Cheers.java
+++ Cheers.java
@@ -17,9 +17,9 @@ public class Cheers {
                          if(name1.charAt(i)=='A'||name1.charAt(i)=='E'||name1.charAt(i)=='F'||name1.charAt(i)=='H'||name1.charAt(i)=='I'||name1.charAt(i)=='L'||name1.charAt(i)=='M'||name1.charAt(i)=='N'||name1.charAt(i)=='O'||name1.charAt(i)=='R'||name1.charAt(i)=='S'||name1.charAt(i)=='X'){
                                 spel="an";
                                 }else{
-                                        spel="a";
+                                        spel="a ";
                                 }
-                        System.out.println("Give me "+spel+ "  "+name1.charAt(i)+": "+ name1.charAt(i)+"!");
+                        System.out.println("Give me "+spel+ " "+name1.charAt(i)+": "+ name1.charAt(i)+"!");
                 }
                 System.out.println("What does that spell?");
                 for (int i =0 ; i<it;i++){
